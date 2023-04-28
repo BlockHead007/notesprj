cd4d13f8427b9107c4abccf61d0a4d8c4ba0f5b3
Archoter <100906478+Archoter@users.noreply.github.com>
2023-04-28 06:21:27 +0300

    Update MonoThreadClientHandler(v0).java

--- MonoThreadClientHandler(v0).java
+++ MonoThreadClientHandler(v0).java
@@ -29,18 +29,12 @@ public class MonoThreadClientHandler implements Runnable {
 
             while (!clientDialog.isClosed()) {
                 System.out.println("Server reading from channel");
-
-                // серверная нить ждёт в канале чтения (inputstream) получения
-                // данных клиента после получения данных считывает их
                 String entry = in.readUTF();
 
-                // и выводит в консоль
                 System.out.println("READ from clientDialog message - " + entry);
 
                 if (entry.equalsIgnoreCase("quit")) {
 
-                    // если кодовое слово получено то инициализируется закрытие
-                    // серверной нити
                     System.out.println("Client initialize connections suicide ...");
                     out.writeUTF("Server reply - " + entry + " - OK");
                     Thread.sleep(3000);
@@ -49,34 +43,22 @@ public class MonoThreadClientHandler implements Runnable {
 
                 if (entry.equalsIgnoreCase("Search")){
                     String SQLcommand = "";
-                    //statement.executeUpdate(SQLcommand);
                 }
-                    // если условие окончания работы не верно - продолжаем работу -
-                // отправляем эхо обратно клиенту
 
                 System.out.println("Server try writing to channel");
                 out.writeUTF("Server reply - " + entry + " - OK");
                 System.out.println("Server Wrote message to clientDialog.");
 
-                // освобождаем буфер сетевых сообщений
                 out.flush();
 
-                // возвращаемся в началло для считывания нового сообщения
             }
 
-            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-            // основная рабочая часть //
-            //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-            // если условие выхода - верно выключаем соединения
             System.out.println("Client disconnected");
             System.out.println("Closing connections & channels.");
 
-            // закрываем сначала каналы сокета !
             in.close();
             out.close();
 
-            // потом закрываем сокет общения с клиентом в нити моносервера
             clientDialog.close();
 
             System.out.println("Closing connections & channels - DONE.");
